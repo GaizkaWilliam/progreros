7fb8d0ab121eaadd6a2a77bc2e300a4c75b68357
GaizkaWilliam <gaizka.ibarzabal@opendeusto.es>
2021-04-04 12:30:29 +0200

    Registro

---
+++ SkapaClothes/include/Registro.h
@@ -0,0 +1,16 @@
+#ifndef REGISTRO_H
+#define REGISTRO_H
+
+
+class Registro
+{
+    public:
+        Registro();
+        virtual ~Registro();
+
+    protected:
+
+    private:
+};
+
+#endif // REGISTRO_H
