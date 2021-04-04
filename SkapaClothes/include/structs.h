abacb44b4dfafd757c406c04ac597119790b0f6c
GaizkaWilliam <gaizka.ibarzabal@opendeusto.es>
2021-04-04 12:11:35 +0200

    Structs.h

---
+++ SkapaClothes/include/structs.h
@@ -0,0 +1,16 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+
+class structs
+{
+    public:
+        structs();
+        virtual ~structs();
+
+    protected:
+
+    private:
+};
+
+#endif // STRUCTS_H
